7b60c42ff06f14a9c63c24c004b62684684dc846
Jashanpreet kaur <jashanpreetkaurkhass@gmail.com>
2026-04-02 10:01:56 +0530

    Time: 165 ms (55.79%), Space: 27 MB (48.98%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -0,0 +1,20 @@
+class Solution:
+    def dailyTemperatures(self, temperatures):
+        n = len(temperatures)
+        answer = [0] * n
+        stack = []
+
+        for i in range(n):
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                idx = stack.pop()
+                answer[idx] = i - idx
+            stack.append(i)
+
+        return answer
+
+
+# test cases
+s = Solution()
+print(s.dailyTemperatures([73,74,75,71,69,72,76,73]))
+print(s.dailyTemperatures([30,40,50,60]))
+print(s.dailyTemperatures([30,60,90]))
