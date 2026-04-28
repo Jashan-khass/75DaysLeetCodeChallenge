7c72b07191096302ee6b1bc9a7bfe95368580eea
Jashanpreet kaur <jashanpreetkaurkhass@gmail.com>
2026-04-28 17:06:17 +0530

    Time: 11 ms (51.29%), Space: 13.5 MB (97.06%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,16 @@
+class Solution:
+    def majorityElement(self, nums):
+        count = 0
+        candidate = None
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += 1 if num == candidate else -1
+        return candidate
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    
+    print(sol.majorityElement([3, 2, 3]))
+    print(sol.majorityElement([2, 2, 1, 1, 1, 2, 2]))
