51d1607de88a74ee192593c8c189bb85e629caa4
Jashanpreet kaur <jashanpreetkaurkhass@gmail.com>
2026-06-13 17:57:29 +0530

    Time: 0 ms (100%), Space: 13.3 MB (36.79%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,9 @@
+class Solution:
+    def missingNumber(self, nums):
+        n = len(nums)
+        return n * (n + 1) // 2 - sum(nums)
+
+sol = Solution()
+print(sol.missingNumber([3, 0, 1]))
+print(sol.missingNumber([0, 1]))
+print(sol.missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]))
